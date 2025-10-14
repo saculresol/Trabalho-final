0b386c6a3c0c4d18731b0e1a282f7011344afd25
YoranHansel <yoranhansel@gmail.com>
2025-10-14 11:15:47 -0300

    V5
    Versão 5

--- TrabalhoFinal/App.js
+++ TrabalhoFinal/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { AntDesign } from '@expo/vector-icons';
 
 import LoginScreen from './Screens/LoginScreen';
 import HomeScreen from './Screens/HomeScreen';
@@ -10,6 +12,68 @@ import TransacoesScreen from './Screens/TransacoesScreen';
 import ConfigScreen from './Screens/ConfigScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#A4BB49',
+        tabBarInactiveTintColor: '#999',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Início',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cantina"
+        component={CantinaScreen}
+        options={{
+          tabBarLabel: 'Cardápio',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="book" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function AppDrawer() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerActiveTintColor: '#A4BB49',
+        drawerInactiveTintColor: '#333',
+      }}
+    >
+      <Drawer.Screen
+        name="HomeTabs"
+        component={HomeTabs}
+        options={{ drawerLabel: 'Home' }}
+      />
+      <Drawer.Screen
+        name="Transacoes"
+        component={TransacoesScreen}
+        options={{ drawerLabel: 'Transações' }}
+      />
+      <Drawer.Screen
+        name="Config"
+        component={ConfigScreen}
+        options={{ drawerLabel: 'Configurações' }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -20,7 +84,11 @@ export default function App() {
           component={LoginScreen}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="AppDrawer"
+          component={AppDrawer}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
