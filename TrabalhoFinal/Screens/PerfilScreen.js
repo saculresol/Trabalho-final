5effb0beacd91aa1a59cb06cb7bd7368225f8501
Henrique20090919 <henriquesouzag16@gmail.com>
2025-11-03 08:52:42 -0300

    Perfil
    Feito a tela de perfil mais estilizada e organizada

--- TrabalhoFinal/Screens/PerfilScreen.js
+++ TrabalhoFinal/Screens/PerfilScreen.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function TransacoesScreen() {  
   return (
     <View style={styles.container}>
-      <Text>Perfil</Text>
+       <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ71x_K2UcszdyBiq6m5BzXxaCwEgCSc74gsQ&s'}}
+       style={{ width: 100, height: 100, borderRadius:100 }}></Image>
+      <Text>Nome:</Text>
+      <Text>Turma:</Text>
       <StatusBar style="auto" />
     </View>
   );
