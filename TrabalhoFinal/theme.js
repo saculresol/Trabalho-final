18d9fbfde31e1f1da61e8805894fcea353945ca9
Lucas Pimentel Bento <lucaspimentelbent@gmail.com>
2025-11-03 09:39:28 -0300

    criei o arquivo dos temas

---
+++ TrabalhoFinal/theme.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  backgroundColor: '#F8F9FA',
+  color: '#2D3142',
+  botaoBg: '#E5E7EB',
+  botaoTexto: '#1F2937',
+  borda: '#D1D5DB',
+  footerColor: '#9CA3AF',
+  statusBar: 'dark',
+};
+
+export const darkTheme = {
+  backgroundColor: '#1F2937',
+  color: '#F9FAFB',
+  botaoBg: '#374151',
+  botaoTexto: '#F9FAFB',
+  borda: '#4B5563',
+  footerColor: '#D1D5DB',
+  statusBar: 'light',
+};
